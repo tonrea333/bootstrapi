542a798867e2e4f0b64c03ba6a587eb4e4841c2d
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-29 10:53:08 -0400

    all

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 const randomBoot = document.querySelector(".btn.btn-secondary");
 const randomPicture = document.querySelector(".card-img-top");
+const tempLocation = document.querySelector('input[type="text"]');
+const tempButton = document.querySelector(".btn.btn-weather");
 
 console.log (randomPicture)
 randomBoot.addEventListener("click", onClick);
@@ -19,5 +21,15 @@ const x = data.message
 
 randomPicture.src = x
 })
+}
 
+tempButton.addEventListener("click", weatherClick);
+
+function weatherClick(){
+console.log("click")
+//function to remove text in weather input field
+
+function removeText(tempLocation) {
+    tempLocation.value = "";
+} removeText(tempLocation)
 }
