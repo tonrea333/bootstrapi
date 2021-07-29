9d5ace2674f1eec07e7a17a0cf75afc51f0a933b
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-29 11:48:03 -0400

    all

--- main.js
+++ main.js
@@ -1,35 +1,60 @@
 
 const randomBoot = document.querySelector(".btn.btn-secondary");
 const randomPicture = document.querySelector(".card-img-top");
-const tempLocation = document.querySelector('input[type="text"]');
-const tempButton = document.querySelector(".btn.btn-weather");
+const tempButton = document.querySelector("#weatherbn");
 
-console.log (randomPicture)
+console.log(randomPicture)
 randomBoot.addEventListener("click", onClick);
 
-function onClick(){
+function onClick() {
 
 
-fetch("https://dog.ceo/api/breeds/image/random")
-.then(function(bootdata){
-    return bootdata.json();
-})
-.then(function (data){
-console.log(data, "data")
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(function (bootdata) {
+            return bootdata.json();
+        })
+        .then(function (data) {
+            console.log(data, "data")
 
-const x = data.message
+            const x = data.message
 
-randomPicture.src = x
-})
+            randomPicture.src = x
+        })
 }
 
 tempButton.addEventListener("click", weatherClick);
 
-function weatherClick(){
-console.log("click")
-//function to remove text in weather input field
+function weatherClick() {
+    const tempLocation = document.querySelector('input[type="text"]');
+    const temperatureWeather = document.querySelector("#temperature")
+    const windWeather = document.querySelector("#wind");
+    const forecastWeather = document.querySelector("forecast");
+    const descriptionWeather = document.querySelector("description");
+    const tempValue = tempLocation.value;
+    console.log(tempValue, "value")
+    const cityHold = "https://goweather.herokuapp.com/weather/{tempValue}";
+    console.log(cityHold, 'weather')
 
-function removeText(tempLocation) {
-    tempLocation.value = "";
-} removeText(tempLocation)
+    fetch(cityHold)
+        .then(function (bootdata) {
+            return bootdata.json();
+        })
+        .then(function (data) {
+            console.log(data, "weather")
+           console.log(data.temperature)
+            data.wind;
+            data.description;
+             data.forecast;
+            
+            
+
+
+
+        })
+
+    //function to remove text in weather input field
+
+    function removeText(tempLocation) {
+      tempLocation.value = "";
+    } removeText(tempLocation)
 }
